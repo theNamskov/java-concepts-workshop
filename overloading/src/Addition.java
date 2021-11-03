b7165950a7db2706badf76ec1952074409118817
thenamskov <ahoryaw1@outlook.com>
2021-11-03 15:14:01 +0000

    added final submissions to problems

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -3,8 +3,24 @@
  * Please make your changes in this class.
  * Use function overloading to implement TWO methods which prints out the sum of the inputs.
  */
+import java.util.stream.*;
+import java.util.*;
 
 public class Addition {
 
+    public Addition(){}
+
+    public Integer add(Integer ...values) {
+        return new ArrayList<Integer>(Arrays.asList(values)).stream().reduce(0, (sum, val) -> sum+val);
+    }
+
+    /* Didn't work */
+//    public Float add(Float ...values) {
+//        return new ArrayList<Float>(Arrays.asList(values)).stream().reduce(0.0f, (sum, val) -> sum+val);
+//    }
+
+    public Float add(float a, float b, float c) {
+        return a + b + c;
+    }
 }
 
---
+++ overriding/src/FiveASide.java
@@ -0,0 +1,8 @@
+public class FiveASide extends Football {
+    public FiveASide() {
+        super();
+        super.getNumberOfTeamMembers();
+        System.out.println("Football");
+    }
+
+}
